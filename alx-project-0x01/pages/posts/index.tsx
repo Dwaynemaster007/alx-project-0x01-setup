bfdcc99c6c7a9ece223f16a3af1ba1a9596fcaa8
Dwayne Master 007 <thubelihledlamini88@gmail.com>
2025-09-21 16:52:40 +0200

    Fixed Task 2 grader error: Ensured import { PostProps } in pages/posts/index.tsx

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,7 +1,7 @@
+import { PostData, PostProps } from '../../interfaces';
 import PostCard from '../../components/common/PostCard';
 import PostModal from '../../components/common/PostModal';
 import Header from '../../components/layout/Header';
-import { PostData, PostProps } from '../../interfaces';
 import { useState } from 'react';
 
 const Posts: React.FC<PostProps[]> = ({ posts }) => {
@@ -17,11 +17,11 @@ const Posts: React.FC<PostProps[]> = ({ posts }) => {
       <Header />
       <main className="p-4">
         <div className="flex justify-between">
-          <h1 className=" text-2xl font-semibold">Post Content</h1>
+          <h1 className="text-2xl font-semibold">Post Content</h1>
           <button onClick={() => setModalOpen(true)}
             className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
         </div>
-        <div className="grid grid-cols-3 gap-2 ">
+        <div className="grid grid-cols-3 gap-2">
           {
             posts.map(({ title, body, userId, id }: PostProps, key: number) => (
               <PostCard title={title} body={body} userId={userId} id={id} key={key} />
