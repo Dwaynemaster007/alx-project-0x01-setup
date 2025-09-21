3a768ea4a6e55fd91d24096a70c49e8c61e31f56
Dwayne Master 007 <thubelihledlamini88@gmail.com>
2025-09-21 09:22:45 +0200

    Fixed Task 3: Used relative paths for posts and users to resolve module not found errors

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,17 +1,47 @@
-import React from 'react';
-// Make sure the path is correct and the file exists
+import UserCard from '../../components/common/UserCard';
 import Header from '../../components/layout/Header';
+import { UserProps } from '../../interfaces';
 
-const UsersPage: React.FC = () => {
+const Users: React.FC<UserProps[]> = ({ posts }) => {
   return (
     <div className="flex flex-col h-screen">
       <Header />
-      <main className="p-4 flex-grow">
-        <h1 className="text-3xl font-bold">Users</h1>
-        {/* Users will be displayed here */}
+      <main className="p-4">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-semibold">User Content</h1>
+          <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add User</button>
+        </div>
+        <div className="grid grid-cols-3 gap-2">
+          {
+            posts?.map(({ id, name, username, email, address, phone, website, company }: UserProps, key: number) => (
+              <UserCard
+                id={id}
+                name={name}
+                username={username}
+                email={email}
+                address={address}
+                phone={phone}
+                website={website}
+                company={company}
+                key={key}
+              />
+            ))
+          }
+        </div>
       </main>
     </div>
   );
-};
+}
 
-export default UsersPage;
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
+export default Users;
