cb4e7019510ade54c5f9f958b098d61034f2acfe
Dwayne Master 007 <thubelihledlamini88@gmail.com>
2025-09-21 09:50:04 +0200

    Completed Task 5: Added UserModal component and updated users page with modal functionality

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,15 +1,25 @@
 import UserCard from '../../components/common/UserCard';
+import UserModal from '../../components/common/UserModal';
 import Header from '../../components/layout/Header';
-import { UserProps } from '../../interfaces';
+import { UserData, UserProps } from '../../interfaces';
+import { useState } from 'react';
 
 const Users: React.FC<UserProps[]> = ({ posts }) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [user, setUser] = useState<UserData | null>(null);
+
+  const handleAddUser = (newUser: UserData) => {
+    setUser({ ...newUser, id: posts.length + 1 });
+  };
+
   return (
     <div className="flex flex-col h-screen">
       <Header />
       <main className="p-4">
         <div className="flex justify-between">
           <h1 className="text-2xl font-semibold">User Content</h1>
-          <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add User</button>
+          <button onClick={() => setModalOpen(true)}
+            className="bg-blue-700 px-4 py-2 rounded-full text-white">Add User</button>
         </div>
         <div className="grid grid-cols-3 gap-2">
           {
@@ -29,6 +39,9 @@ const Users: React.FC<UserProps[]> = ({ posts }) => {
           }
         </div>
       </main>
+      {isModalOpen && (
+        <UserModal onClose={() => setModalOpen(false)} onSubmit={handleAddUser} />
+      )}
     </div>
   );
 }
